6b38da61d6dd63baa94c0ec5504b4e023aceb81b
Bryan Scott <bryan@pajato.com>
2022-12-23 15:53:59 -0500

    chore: add remaining vagabond abilities
    also correctly mark the infusion effect property to be optional

--- src/data/icon/types/infuse.ts
+++ src/data/icon/types/infuse.ts
@@ -3,7 +3,7 @@ import {IconAttack} from "./attack";
 export type IconInfusion = {
     cost: number
     name: string
-    effect: string
+    effect?: string
     range?: string
     attack?: IconAttack
     area?: string
