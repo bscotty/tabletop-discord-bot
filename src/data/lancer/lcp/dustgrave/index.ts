785dc0e52d6da0af694933e575b0ea9526849f9d
Bryan Scott <bryan@pajato.com>
2023-07-23 13:47:26 -0400

    chore: update first party lcps

--- src/data/lancer/lcp/dustgrave/index.ts
+++ src/data/lancer/lcp/dustgrave/index.ts
@@ -2,6 +2,7 @@ import coreBonuses from "./core_bonuses.json"
 import frames from "./frames.json"
 import info from "./lcp_manifest.json"
 import pilot_gear from "./pilot_gear.json"
+import reserves from "./reserves.json"
 import systems from "./systems.json"
 import talents from "./talents.json"
 import weapons from "./weapons.json"
@@ -21,7 +22,7 @@ export function getDustgraveLcp(): Lcp {
         [],
         [],
         pilot_gear,
-        [],
+        reserves,
         undefined,
         [],
         [],
