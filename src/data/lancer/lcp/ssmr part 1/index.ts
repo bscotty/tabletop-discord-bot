1ea8d80c2b0a8e0120a5539470563cf3406dd0e1
sarah <sealtrowitz@gmail.com>
2023-10-13 15:51:47 -0500

    chore: update ssmr lcp

--- src/data/lancer/lcp/ssmr part 1/index.ts
+++ src/data/lancer/lcp/ssmr part 1/index.ts
@@ -1,6 +1,8 @@
 import {Lcp} from "../../types/lcp";
 import frames from "./frames.json"
 import info from "./lcp_manifest.json"
+import pilot_gear from "./pilot_gear.json"
+import systems from "./systems.json"
 import talents from "./talents.json"
 import weapons from "./weapons.json"
 
@@ -17,13 +19,13 @@ export function getSsmrPart1Lcp(): Lcp {
         info,
         [],
         [],
-        [],
+        pilot_gear,
         [],
         undefined,
         [],
         [],
         [],
-        [],
+        systems,
         undefined,
         [],
         talents,
