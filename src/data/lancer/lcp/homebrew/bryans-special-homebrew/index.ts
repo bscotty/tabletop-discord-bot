73f98718f60f2d170bdb1b1b29f970964978dd10
Bryan Scott <bryan@pajato.com>
2024-01-11 19:38:09 -0500

    chore: add the bsh homebrew

---
+++ src/data/lancer/lcp/homebrew/bryans-special-homebrew/index.ts
@@ -0,0 +1,32 @@
+import {Lcp} from "../../../types/lcp";
+import frames from "./frames.json"
+import info from "./lcp_manifest.json"
+import systems from "./systems.json"
+import weapons from "./weapons.json"
+
+export function getBryansSpecialHomebrewLcp(): Lcp {
+    return new Lcp(
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        frames,
+        [],
+        info,
+        [],
+        [],
+        [],
+        [],
+        undefined,
+        [],
+        [],
+        [],
+        systems,
+        undefined,
+        [],
+        [],
+        weapons
+    )
+}
