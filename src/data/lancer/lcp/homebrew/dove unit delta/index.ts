e221cb1e2832441ccb140bb06cbcae2f02dfe42e
sarah <sealtrowitz@gmail.com>
2024-03-14 20:44:40 -0500

    chore: add reserves to index.ts in dove unit delta 1.0.0 lcp

--- src/data/lancer/lcp/homebrew/dove unit delta/index.ts
+++ src/data/lancer/lcp/homebrew/dove unit delta/index.ts
@@ -2,6 +2,7 @@ import {Lcp} from "../../../types/lcp";
 import info from "./lcp_manifest.json"
 import pilotGear from "./pilot_gear.json"
 import bonds from "./bonds.json"
+import reserves from "./reserves.json"
 
 export function getDoveUnitDeltaLcp(): Lcp {
     return new Lcp(
@@ -17,7 +18,7 @@ export function getDoveUnitDeltaLcp(): Lcp {
         [],
         [],
         pilotGear,
-        [],
+        reserves,
         undefined,
         [],
         [],
