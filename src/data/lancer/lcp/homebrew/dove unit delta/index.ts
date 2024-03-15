fe39d86b0e771f1d71fe948cd72e8767d210c943
sarah <sealtrowitz@gmail.com>
2024-03-14 20:08:54 -0500

    chore: add dove unit delta 1.0.0 lcp

---
+++ src/data/lancer/lcp/homebrew/dove unit delta/index.ts
@@ -0,0 +1,31 @@
+import {Lcp} from "../../types/lcp";
+import info from "./lcp_manifest.json"
+import pilotGear from "./pilot_gear.json"
+import bonds from "./bonds.json"
+
+export function getDoveUnitDeltaLcp(): Lcp {
+    return new Lcp(
+        [],
+        [],
+        bonds,
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        pilotGear,
+        [],
+        undefined,
+        [],
+        [],
+        [],
+        [],
+        undefined,
+        [],
+        [],
+        []
+    )
+}
