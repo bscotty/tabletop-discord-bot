147d8651efb6a3e506ddf5f6eb341306dbc609e0
Bryan Scott <bryan@pajato.com>
2024-03-23 10:33:27 -0400

    chore: add build check

--- src/commands/godbound.ts
+++ src/commands/godbound.ts
@@ -1,5 +1,5 @@
 import Searcher from "../data/searcher";
-import {getDictionary} from "../data/godbound/words";
+import {getDictionary} from "../data/godbound/words/true-index";
 import {formatGodbound} from "../data/godbound/formatter";
 import {DivineItem} from "../data/godbound/divine-item";
 import {SearchCommand} from "./searchCommand";
@@ -9,9 +9,15 @@ export class Godbound extends SearchCommand<DivineItem> {
 
     constructor() {
         super("godbound", "Search for a term in Godbound RPG");
-        const dictionary = getDictionary()
-        const items = [...dictionary.words, ...dictionary.gifts, ...dictionary.invocations]
-        this.searcher = new Searcher(items, ["name"])
+        try {
+            const dictionary = getDictionary()
+            const items = [...dictionary.words, ...dictionary.gifts, ...dictionary.invocations]
+            this.searcher = new Searcher(items, ["name"])
+        } catch (e) {
+            console.error(`Something went wrong`, e)
+            this.searcher = new Searcher([], ["name"])
+        }
+
     }
 
     override format(item: DivineItem): string {
