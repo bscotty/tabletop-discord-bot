ea0715e43ba517e75e1cfb067f97b54cd8869795
Bryan Scott <bryan@pajato.com>
2024-04-03 20:44:28 -0400

    feat: add requested features to formatting
    * list bond powers for bond printout
    * safely handle specialty licenses
    * replace plaintext statistics box with set of fields

--- src/data/lancer/format/rich-frame-formatter.ts
+++ src/data/lancer/format/rich-frame-formatter.ts
@@ -1,7 +1,7 @@
 import {SearchableFrame} from "../search/searchable";
 import TurndownService from "turndown";
 import {LicenseData, Repository} from "./repository";
-import {Formatters} from "./formatters";
+import {Formatters, ZERO_SPACE} from "./formatters";
 import {formatContentPack} from "./format-utility";
 import {FrameStats, IFrameTraitData} from "../types/frame";
 import {RichFormatter} from "./rich-formatter";
@@ -22,7 +22,14 @@ export class RichFrameFormatter implements RichFormatter<SearchableFrame> {
     }
 
     richFormat(item: SearchableFrame): DisplayResponse {
-        const frame = item
+        if (item.specialty) {
+            return this.specialtyFormat(item)
+        } else {
+            return this.frameFormat(item)
+        }
+    }
+
+    private frameFormat(frame: SearchableFrame): DisplayResponse {
         const {stats, core_system} = frame
         const coreName = core_system.name || core_system.passive_name || core_system.active_name
 
@@ -38,10 +45,41 @@ export class RichFrameFormatter implements RichFormatter<SearchableFrame> {
             fields: [
                 {name: "Size", description: `${frame.stats.size}`, inline: true},
                 {name: "Mounts", description: `${frame.mounts.join(', ')}`, inline: true},
-                {name: "Statistics", description: this.formattedStatBlock(stats), inline: false},
+                ...this.formattedStatFields(stats),
                 ...(frame.traits.map((trait) => this.traitToField(frame.source, trait))),
                 {name: "Core System", description: coreName, inline: false},
                 ...this.getLicenseGear(frame)
+            ],
+            buttons: []
+        }
+    }
+
+    private specialtyFormat(specialtyLicense: SearchableFrame): DisplayResponse {
+        console.log(`found specialty item: ${specialtyLicense.id}`)
+        const {imageUrl, file} = getManufacturerLogo(specialtyLicense.source, this.repo)
+        const color = getColor(specialtyLicense.source, this.repo)
+        const prerequisite: ResponseField[] = []
+        const specialty = specialtyLicense.specialty
+        if (typeof specialty != "boolean") {
+            prerequisite.push(
+                {
+                    name: "Prerequisite",
+                    description: `${specialty.min_rank} Rank(s) of ${specialty.source} Licenses`,
+                    inline: false
+                }
+            )
+        }
+        const mechType = specialtyLicense.mechtype.length > 0 ? specialtyLicense.mechtype.join("/") : "Specialty"
+        return {
+            color: color,
+            authorName: `${specialtyLicense.source} ${specialtyLicense.name}`,
+            authorIconUrl: imageUrl,
+            thumbnailUrl: specialtyLicense.image_url || "https://d2c79xe1p61csc.cloudfront.net/frames/nodata.png",
+            description: `${mechType}${formatContentPack(specialtyLicense)}`,
+            localAssetFilePaths: file ? [file] : [],
+            fields: [
+                ...prerequisite,
+                ...this.getLicenseGear(specialtyLicense)
             ],
             buttons: []
         }
@@ -59,41 +97,30 @@ export class RichFrameFormatter implements RichFormatter<SearchableFrame> {
         return {name: trait.name, description: description.trim(), inline: false}
     }
 
-    private formattedStatBlock(stats: FrameStats): string {
-        const horizontalLine: string = "|-------------|-------------|-------------|"
+    private formattedStatFields(stats: FrameStats): ResponseField[] {
         const techAttack = stats.tech_attack > 0 ? `+${stats.tech_attack}` : `${stats.tech_attack}`
-
-        function assembleRow(first: string, second: string, third: string): string {
-            function statWhitespace(value: string): string {
-                const maxSize = 13
-                const whiteSpace = maxSize - value.length
-                const startingWhitespace = Math.round(whiteSpace / 2)
-                return " ".repeat(startingWhitespace) + value + " ".repeat(whiteSpace - startingWhitespace)
-            }
-
-            return "|" + [first, second, third].map((it) => statWhitespace(it)).join("|") + "|"
-        }
-
         return [
-            "```",
-            horizontalLine,
-            assembleRow("STRUCTURE", "STRESS", "ARMOR"),
-            assembleRow(`${stats.structure}`, `${stats.stress}`, `${stats.armor}`),
-            horizontalLine,
-            assembleRow("HP", "EVASION", "E-DEF"),
-            assembleRow(`${stats.hp}`, `${stats.evasion}`, `${stats.edef}`),
-            horizontalLine,
-            assembleRow("HEATCAP", "SENSORS", "TECH ATTACK"),
-            assembleRow(`${stats.heatcap}`, `${stats.sensor_range}`, `${techAttack}`),
-            horizontalLine,
-            assembleRow("REPAIR CAP", "SAVE", "SPEED"),
-            assembleRow(`${stats.repcap}`, `${stats.save}`, `${stats.speed}`),
-            horizontalLine,
-            assembleRow("", "SP", ""),
-            assembleRow("", `${stats.sp}`, ""),
-            horizontalLine,
-            "```"
-        ].join("\n")
+            {name: ZERO_SPACE, description: `**-------------- STATISTICS --------------**`, inline: false},
+
+            {name: "STRUCTURE", description: `${stats.structure}`, inline: true},
+            {name: "STRESS", description: `${stats.stress}`, inline: true},
+            {name: "ARMOR", description: `${stats.armor}`, inline: true},
+
+            {name: "HP", description: `${stats.hp}`, inline: true},
+            {name: "EVASION", description: `${stats.evasion}`, inline: true},
+            {name: "E-DEF", description: `${stats.edef}`, inline: true},
+
+            {name: "HEATCAP", description: `${stats.heatcap}`, inline: true},
+            {name: "SENSORS", description: `${stats.sensor_range}`, inline: true},
+            {name: "TECH ATTACK", description: `${techAttack}`, inline: true},
+
+            {name: "REPAIR CAP", description: `${stats.repcap}`, inline: true},
+            {name: "SAVE", description: `${stats.save}`, inline: true},
+            {name: "SPEED", description: `${stats.speed}`, inline: true},
+
+            {name: "SP", description: `${stats.sp}`, inline: true},
+            {name: ZERO_SPACE, description: ZERO_SPACE, inline: false},
+        ]
     }
 
     private getLicenseGear(frame: SearchableFrame): ResponseField[] {
@@ -106,17 +133,20 @@ export class RichFrameFormatter implements RichFormatter<SearchableFrame> {
             return []
         }
 
-        function getGearNames(forLicenseLevel: 1 | 2 | 3, gear: LicenseData[]): string {
+        function getGearNames(forLicenseLevel: 1 | 2 | 3, gear: LicenseData[]): string[] {
             return gear.filter((it) => it.license_level == forLicenseLevel)
                 .map((it) => isSearchableFrame(it) ? `${it.name} Frame` : it.name)
-                .join(", ")
         }
 
+        const validLicenseLevels: (1 | 2 | 3)[] = [1, 2, 3]
         const gear = this.repo.getLicenseData(frame)
-        return [
-            {name: "License Level 1", description: getGearNames(1, gear), inline: true},
-            {name: "License Level 2", description: getGearNames(2, gear), inline: true},
-            {name: "License Level 3", description: getGearNames(3, gear), inline: true}
-        ]
+
+        return validLicenseLevels.map((licenseLevel) => {
+            return ({level: licenseLevel, names: getGearNames(licenseLevel, gear)});
+        }).filter(({names}) => {
+            return names.length > 0
+        }).map(({level, names}) => {
+            return ({name: `License Level ${level}`, description: `${names.join(", ")}`, inline: true})
+        })
     }
 }
