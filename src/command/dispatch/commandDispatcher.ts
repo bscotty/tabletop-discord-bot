cadb89f99bd43079914aab08c2ee4335da131138
Amu <afoxparadox@gmail.com>
2026-09-24 19:07:24 -0400

    fix: added handling of invalid guildId for command dispatch

--- src/command/dispatch/commandDispatcher.ts
+++ src/command/dispatch/commandDispatcher.ts
@@ -1,5 +1,6 @@
 import {BaseInteraction} from "discord.js";
 import SlashCommand from "../slashCommand";
+import Config from "../../config/config";
 
 export interface CommandDispatcher {
     dispatch(interaction: BaseInteraction): Promise<void>
@@ -7,12 +8,16 @@ export interface CommandDispatcher {
 
 export class CommandDispatcherImpl implements CommandDispatcher {
     constructor(
+        private readonly config: Config,
         private readonly commands: SlashCommand[]
     ) {
     }
 
     async dispatch(interaction: BaseInteraction) {
-        if (interaction.isCommand()) {
+        if (!this.config.guildIds.includes(interaction.guildId)) {
+            console.error(`Invalid guildId ${interaction.guildId}`);
+        }
+        else if (interaction.isCommand()) {
             const command = this.commands.find((it) => it.name == interaction.commandName)
             if (command != undefined) {
                 try {
